306e845d06c8ec7d682ef057a52e2c9281bd1047
Jackson Fonseca <99935653+JackTrevor@users.noreply.github.com>
2023-02-23 18:46:13 -0300

    Add files via upload
    upda

---
+++ lesson8/scripts/lastvisit.js
@@ -0,0 +1,30 @@
+let moment = new Date();
+
+
+if (localStorage.getItem('lastVisit')) {
+  
+  let lastVisit = new Date(localStorage.getItem('lastVisit'));
+  
+  
+  let diffInMs = moment - lastVisit;
+  let diffInDays = Math.round(diffInMs / (1000 * 60 * 60 * 24));
+  
+  
+  let lastVisitElement = document.getElementById('last-visit');
+  
+  if (diffInDays === 0) {
+    
+    lastVisitElement.textContent = 'today';
+  } else if (diffInDays === 1) {
+    
+    lastVisitElement.textContent = 'yesterday';
+  } else {
+    
+    lastVisitElement.textContent = diffInDays + ' days ago';
+  }
+}
+
+localStorage.setItem('lastVisit', moment);
+
+
+  
