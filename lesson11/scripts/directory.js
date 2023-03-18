77663d31da1125fb76c62e8a66769982e4d1eb84
Jack Trevor <sbrobools@gmail.com>
2023-03-18 16:05:07 -0300

    changings to spottlights

--- lesson11/scripts/directory.js
+++ lesson11/scripts/directory.js
@@ -52,15 +52,7 @@ const gridButton = document.querySelector("#grid");
 const listButton = document.querySelector("#list");
 const display = document.querySelector("section");
 
-// gridButton.addEventListener("click", () => {
-//     display.classList.add("grid");
-//     display.classList.remove("list");
-// });
 
-// listButton.addEventListener("click", () => {
-//     display.classList.add("list");
-//     display.classList.remove("grid");
-// })
 
 gridButton.addEventListener("click", () => {
 	// example using arrow function
